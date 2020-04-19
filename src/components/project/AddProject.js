282733de15134531167e67bbabcaf2cbe3ca8aaa
Mohamed Sameh <msmaeh.93@hotmail.com>
2020-04-19 18:56:11 +0200

    Add project form - Component states - controlled input

--- src/components/project/AddProject.js
+++ src/components/project/AddProject.js
@@ -1,10 +1,77 @@
 import React, { Component } from "react";
 
 class AddProject extends Component {
+    constructor(props) {
+        super(props);
+        //initial state
+        this.state= {
+            name: "",
+            identifier: "",
+            description: "",
+            startDate: "",
+            endDate: ""
+        }
+        this.onChange= this.onChange.bind(this);
+        this.onSubmit= this.onSubmit.bind(this);
+    }
+    onChange(e) {   //this.onChange.bind(this) bound on tag
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    }
+    onSubmit(e) {
+        e.preventDefault();
+        const newProjectObj= this.state;    //this case we will not have more states than project form
+        //better to assign each key to each value
+        console.log(newProjectObj);
+    }
     render() {
         return (
-            <div>
-                <h1>Add project Form</h1>
+            <div>  
+                <div className="register">
+                    <div className="container">
+                        <div className="row">
+                            <div className="col-md-8 m-auto">
+                                <h5 className="display-4 text-center">Create Project form</h5>
+                                <hr />
+                                <form onSubmit={this.onSubmit}>
+                                    <div className="form-group">
+                                        <input type="text" className="form-control form-control-lg " placeholder="Project Name"
+                                        name="name"
+                                        value={this.state.name}
+                                        onChange={this.onChange} />
+                                    </div>
+                                    <div className="form-group">
+                                        <input type="text" className="form-control form-control-lg" placeholder="Unique Project ID"
+                                        name="identifier"
+                                        value={this.state.identifier}
+                                        onChange={this.onChange}/>
+                                    </div>
+                                    <div className="form-group">
+                                        <textarea className="form-control form-control-lg" placeholder="Project Description"
+                                        name="description"
+                                        value={this.state.description}
+                                        onChange={this.onChange}></textarea>
+                                    </div>
+                                    <h6>Start Date</h6>
+                                    <div className="form-group">
+                                        <input type="date" className="form-control form-control-lg" name="startDate"
+                                        value={this.state.startDate}
+                                        onChange={this.onChange}/>
+                                    </div>
+                                    <h6>Estimated End Date</h6>
+                                    <div className="form-group">
+                                        <input type="date" className="form-control form-control-lg" name="endDate"
+                                        value={this.state.endDate}
+                                        onChange={this.onChange}/>
+                                    </div>
+            
+                                    <input type="submit" className="btn btn-primary btn-block mt-4" />
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     };
